08ca0982ac429829460cdf29d93a10e964b9c3e3
Nuwan Chamara <nuwanlanka@gmail.com>
2015-05-21 05:49:23 +0530

    Java - Concurrency - Starting A Background Task

--- src/main/java/com/nuwan/web/javaExamples/concurrency/StartingABackgroundTaskController.java
+++ src/main/java/com/nuwan/web/javaExamples/concurrency/StartingABackgroundTaskController.java
@@ -18,11 +18,22 @@ public class StartingABackgroundTaskController {
 
     private ArrayList<String> outputArray;
 
+//    Within the JVM are two types of threads: User and Daemon. User threads keep executing until their run()
+//    method completes, whereas Daemon threads can be terminated if the application needs to exit. An application exits if
+//    there are only Daemon threads running in the JVM.
+//    Usually, Daemon threads will have a Runnable interface that doesn’t complete; for example a while (true) loop.
+//    This allows these threads to periodically check or perform a certain condition throughout the life of the program, and
+//    be discarded when the program is finished executing.
+//    To set a thread as a Daemon thread, use thread.setDaemon(true) before calling the thread.start() method.
+//    By default, Thread instances are created as User thread types.
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
     public List<String> startingABackgroundTask() {
         outputArray = new ArrayList<>();
 
+//        The new thread created is a User
+//        thread, which means that the application will not exit until both the main thread and the background thread are
+//        done executing.
         Thread backgroundThread = new Thread(new Runnable() {
             public void run() {
                 doSomethingInBackground();
